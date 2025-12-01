ae614ffc1f8f7edf936bad587153fa66632f54bd
James Brunton <jamedbrunton@gmail.com>
2025-11-30 20:12:48 -0800

    refactoring... very messy at the moment

--- las_structuring/data.py
+++ las_structuring/data.py
@@ -1,8 +1,7 @@
 from dataclasses import dataclass
 import pandas as pd
-from RandLANetImplementation.constants import HYPER_PARAMETERS, TRAIN_PER_HOLDOUT, POSITIVE_CLASS_DIR, NEGATIVE_CLASS_DIR, TRAIN_PER_TEST_FOLD
-from typing import List, Tuple
-from RandLANetImplementation.constants import HYPER_PARAMETERS, TRAIN_PER_HOLDOUT, POSITIVE_CLASS_DIR, NEGATIVE_CLASS_DIR, TRAIN_PER_TEST_FOLD
+import numpy as np
+from RandLANetImplementation.constants import HYPER_PARAMETERS, TRAIN_PER_HOLDOUT, POSITIVE_CLASS_DIR, NEGATIVE_CLASS_DIR, TRAIN_PER_TEST_FOLD, KEEP_DIMS
 from typing import List, Tuple
 import torch
 from sklearn.cluster import KMeans
@@ -11,17 +10,27 @@ import laspy
 def load_combined_pos_neg_df() -> pd.DataFrame:
     pos_class = laspy.read(POSITIVE_CLASS_DIR)
     neg_class = laspy.read(NEGATIVE_CLASS_DIR)
-    pos_class = pd.DataFrame({dim.name: pos_class[dim.name] 
-                   for dim in pos_class.point_format.dimensions})
-    neg_class = pd.DataFrame({dim.name: neg_class[dim.name] 
-                   for dim in neg_class.point_format.dimensions})
+    pos_class = pd.DataFrame({
+        dim.name: pos_class[dim.name]
+        for dim in pos_class.point_format.dimensions
+        if dim.name in KEEP_DIMS
+    })
+
+    neg_class = pd.DataFrame({
+        dim.name: neg_class[dim.name]
+        for dim in neg_class.point_format.dimensions
+        if dim.name in KEEP_DIMS
+    })
     pos_class["label"] = 1
     neg_class["label"] = 0
     combined = pd.concat([pos_class, neg_class])
+    combined = combined.infer_objects()
+    combined = pd.concat([pos_class, neg_class])
+    combined = combined.astype(float)
     #TO DO... unfuck this coercsion thing, like we need to make
     #sure we are actually doing this right... for now just fucking
     #stuff everything into numeric...
-    combined = combined.apply(pd.to_numeric, errors = 'coerce')
+    combined = combined.dropna(axis=1)
     return combined
 
 def build_lidar_tensor(las_df: pd.DataFrame) -> torch.Tensor:
@@ -29,17 +38,40 @@ def build_lidar_tensor(las_df: pd.DataFrame) -> torch.Tensor:
     tensor = torch.tensor(las_df.to_numpy(), dtype=torch.float32)
     return tensor
 
-def las_split_kmeans(points: torch.Tensor, split_count=30) -> List[torch.Tensor]:
-    #build folds using kmeans
-    coords = points[:, :2].cpu().numpy() #split using x and y
-    labels = KMeans(n_clusters=split_count).fit_predict(coords)
+def las_split_kmeans(points: torch.Tensor,
+                     split_count: int,
+                     sample_size: int = 100_000) -> List[torch.Tensor]:
+
+    coords = points[:, :2].cpu().numpy().astype(np.float32, copy=False)
+    N = coords.shape[0]
+
+    if N > sample_size:
+        idx = np.random.choice(N, sample_size, replace=False)
+        coords_sample = coords[idx]
+    else:
+        coords_sample = coords
+        idx = np.arange(N)
+
+    km = KMeans(
+        n_clusters=split_count,
+        init="k-means++",
+        n_init=5,
+        max_iter=50,
+        tol=1e-3,
+        algorithm="elkan"
+    )
+
+    km.fit(coords_sample)
+
+    labels = km.predict(coords)
 
     labels = torch.from_numpy(labels).long()
 
     splits = []
     for f in range(split_count):
-        idx = torch.nonzero(labels == f, as_tuple=False).squeeze(1)
-        splits.append(idx) 
+        idx_f = torch.nonzero(labels == f, as_tuple=False).squeeze(1)
+        splits.append(idx_f)
+
     return splits
 
 @dataclass
@@ -74,7 +106,7 @@ class modelData:
         self.folds = self._get_folds(self.train_set, train_per_test_fold)
 
     def new_split(self, train_to_hold_out: int = None, train_test_ratio_fold: int = None):
-        if not self.data:
+        if (self.data == None):
             self.prepare_data()
         self.prepare_hyper_params()
         self.prepare_train_test(train_to_hold_out)
@@ -94,8 +126,6 @@ class modelData:
         splits = las_split_kmeans(data, test_per_train + 1)
         test_idxs = splits[0]
         train_idxs = torch.cat(splits[1:], dim=0)
-        print(test_idxs)
-        print(train_idxs)
         train = data[train_idxs]
         test = data[test_idxs]
         return train, test
@@ -109,3 +139,6 @@ class modelData:
 
     def get_folds(self) -> List[torch.Tensor]:
         return self.folds
+    
+    def get_data_from_fold(self, fold_index: int):
+        return self.test_set[self.folds[fold_index]]
--- quick_and_dirty_models/RandLANetImplementation/constants.py
+++ quick_and_dirty_models/RandLANetImplementation/constants.py
@@ -1,7 +1,7 @@
 # MODEL PARAMS:
 # Large test sets because we can label as much data as we want and our positive class is pretty damn rare
 TRAIN_PER_HOLDOUT = 5
-TRAIN_PER_TEST_FOLD = 5
+TRAIN_PER_TEST_FOLD = 200
 TRAIN_FOLD_TO_TEST_FOLD_RATIO = 0.6
 HYPER_PARAMETERS = {}
 
@@ -11,3 +11,9 @@ WINDOW_SIZE = 500
 
 POSITIVE_CLASS_DIR = "RandLANetImplementation/data/POSITIVE.las"
 NEGATIVE_CLASS_DIR = "RandLANetImplementation/data/NEGATIVE.las"
+
+
+NIEGBBOUR_COUNT = 4
+
+
+KEEP_DIMS = ["x", "y", "z", "intensity"]
